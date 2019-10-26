c16ffd7a7681967e974080cd74d82b630ea41ffd
Chuck Yu <tseyu@yahoo.com>
2019-10-26 11:59:42 -0700

    Add feature to create new blog posts from frontend.

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     # mydjangosite.com/post/2 --> online
     path('post/<int:pk>', views.post_detail, name='post_detail'),
 
+    # 127.0.0.1:8000/post/new --> local
+    # mydjangosite.com/post/new --> online
+    path('post/new', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
+from blog.forms import PostForm
 from blog.models import Post
 
 
@@ -14,3 +16,18 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     stuff_for_frontend = {'post': post}
     return render(request, 'blog/post_detail.html', stuff_for_frontend)
+
+
+def post_new(request):
+    if request.method == 'post':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        stuff_for_frontend = {'form': form}
+    return render(request, 'blog/post_edit.html', stuff_for_frontend)
